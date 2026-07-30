6555d77585b754ae92b789968f35daf070f7aa4a
SOWMIYA MURUGAN <24csb34@karpagamtech.ac.in>
2026-07-30 23:22:24 +0530

    Add: balanced-binary-tree - submission-0

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+    int check(TreeNode* root) {
+
+        if (root == NULL)
+            return 0;
+
+        int l = check(root->left);
+
+        if (l == -1)
+            return -1;
+
+        int r = check(root->right);
+
+        if (r == -1)
+            return -1;
+
+        if (abs(l - r) > 1)
+            return -1;
+
+        return 1 + max(l, r);
+    }
+
+    bool isBalanced(TreeNode* root) {
+        return check(root) != -1;
+    }
+};
