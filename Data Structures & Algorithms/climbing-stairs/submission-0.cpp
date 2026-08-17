11eb7f7fc894f6128d2ae07f8bd557427bf03111
SOWMIYA MURUGAN <24csb34@karpagamtech.ac.in>
2026-08-17 21:02:39 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int dp[46];
+
+    int solve( int n ){
+        if( n==0 || n==1 ){
+            return 1 ;
+        }
+
+        if( dp[n] != -1){
+            return dp[n] ;
+        }
+
+        return dp[n] = solve(n-1) + solve(n-2);
+    }
+
+    int climbStairs(int n) {
+      fill(dp,dp+46,-1);
+      return solve(n);
+    }
+   
+};
