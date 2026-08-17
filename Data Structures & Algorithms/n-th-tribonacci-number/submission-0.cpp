0507655708fd8af698e24d63d42f78310f885cfd
SOWMIYA MURUGAN <24csb34@karpagamtech.ac.in>
2026-08-17 21:11:17 +0530

    Add: n-th-tribonacci-number - submission-0

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int dp[38];
+
+    int solve( int n){
+        if( n==0 || n==1 ){
+            return n ;
+        }
+
+        if( n== 2){
+            return 1 ;
+        }
+
+        if( dp[n] != -1){
+            return dp[n];
+        }
+
+        return dp[n] = solve(n-1) + solve(n-2) + solve(n-3);
+    }
+    int tribonacci(int n) {
+        fill(dp,dp+38,-1);
+        return solve(n);
+    }
+};
