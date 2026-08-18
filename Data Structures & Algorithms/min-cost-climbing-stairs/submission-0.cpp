901f10a0ff88842c229f03e2db8684f6003d0c30
SOWMIYA MURUGAN <24csb34@karpagamtech.ac.in>
2026-08-18 19:46:51 +0530

    Add: min-cost-climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size() ;
+
+        if( n<=0){
+            return 0 ;
+        }
+
+        int dp[n+1];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for(int i=2 ; i<n ; i++){
+            dp[i] = min(cost[i] + dp[i-1] , cost[i]+dp[i-2]);
+        }
+ 
+        dp[n] = min( dp[n-1] , dp[n-2]);
+        return dp[n];
+    }
+};
