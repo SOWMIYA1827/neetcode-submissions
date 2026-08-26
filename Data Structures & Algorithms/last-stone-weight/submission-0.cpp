e6e80390cf908c53f40e59fa7f9e7e0df48f53db
SOWMIYA MURUGAN <24csb34@karpagamtech.ac.in>
2026-08-26 13:41:38 +0530

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        int n = stones.size();
+
+         priority_queue<int> pq ;
+
+         for(int i=0 ; i<n ;i++){
+            pq.push(stones[i]);
+         }
+
+         while(!pq.empty() && pq.size()>1){
+            int x = pq.top();
+            pq.pop();
+            int y = pq.top();
+            pq.pop();
+
+            if( x!=y){
+                int z = x-y ;
+                pq.push(z);
+            }
+         }
+
+         if(pq.empty()){
+            return 0;
+         }
+
+         return pq.top();
+    
+    }
+};
