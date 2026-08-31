d68500ab26b37939b9bf9fca709a8e67ca7704f0
SOWMIYA MURUGAN <24csb34@karpagamtech.ac.in>
2026-08-31 15:40:46 +0530

    Add: missing-number - submission-2

---
+++ Data Structures & Algorithms/missing-number/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        
+        sort(nums.begin() , nums.end());
+        int ans = 0 ;
+        for(int i=0 ; i<nums.size() ; i++){
+            if( i != nums[i]){
+                ans = i ;
+                return ans ;
+            }
+        }
+
+        return nums.size() ;
+    }
+};
